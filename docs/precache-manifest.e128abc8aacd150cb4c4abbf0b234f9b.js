4f8150a9332082cd308879fea84cf1b2f73c75b8
jdjahj <>
2020-02-22 14:30:45 +0800

    init

---
+++ docs/precache-manifest.e128abc8aacd150cb4c4abbf0b234f9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea4cdd14d0cfa4a76f7bb2c2bb501173",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3db22149178b50a032a4",
+    "url": "/static/css/2.62da32fe.chunk.css"
+  },
+  {
+    "revision": "72cc9147857707a12eb1",
+    "url": "/static/css/main.a16c8361.chunk.css"
+  },
+  {
+    "revision": "3db22149178b50a032a4",
+    "url": "/static/js/2.c458104b.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/static/js/2.c458104b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72cc9147857707a12eb1",
+    "url": "/static/js/main.ad96959b.chunk.js"
+  },
+  {
+    "revision": "0920582e32010ce36cf3",
+    "url": "/static/js/runtime-main.489c3975.js"
+  }
+]);
